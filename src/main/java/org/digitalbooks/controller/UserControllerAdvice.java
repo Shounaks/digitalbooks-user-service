6890dfacc5019d0058852e254dfc5147a47679fc
Shalnark <65479699+Shounaks@users.noreply.github.com>
2023-01-05 20:37:39 +0530

    Adding General Exception Catching

--- src/main/java/org/digitalbooks/controller/UserControllerAdvice.java
+++ src/main/java/org/digitalbooks/controller/UserControllerAdvice.java
@@ -9,11 +9,17 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class UserControllerAdvice {
     @ExceptionHandler(UserServiceException.class)
     public ResponseEntity<ErrorInfo> handleBookServiceException(UserServiceException ex){
         ErrorInfo errorInfo = ErrorInfo.builder().errorId(ex.getId()).errorMessage(ex.getMessage()).build();
+        return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorInfo> handleGeneralError(Exception ex){
+        ErrorInfo errorInfo = ErrorInfo.builder().errorId(0L).errorMessage(ex.getMessage()).build();
         return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
     }
 }
